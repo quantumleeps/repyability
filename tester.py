b9030df5919eab32904289f0c7b506f4631984db
Dan Leeper <dleeper@cwco.com>
2017-07-14 16:34:50 -0400

    Start writing main body of iterative code -now have a node class -trying to shut off systems once their parent shuts off if the node gets shutdown as a result of a component shutting off

--- tester.py
+++ tester.py
@@ -1,29 +1,43 @@
 from component import Component
 from component import Distribution
+from treenode import treeNode
 from math import floor
 
 # Add a component
 engine = Component(Distribution(400, 10), Distribution(12, 1.8), 'Engine', comp_type='np')
+gearbox = Component(Distribution(410, 20), Distribution(8, 0.8), 'Gearbox', comp_type='np')
+hp_pump = Component(Distribution(410, 20), Distribution(8, 0.8), 'HP Pump', comp_type='np')
+diesel_skid = treeNode('Diesel Skid', 'AND', engine, gearbox, hp_pump)
+
+px_array = Component(Distribution(400, 10), Distribution(12, 1.8), 'PXs', comp_type='np')
+membrane = Component(Distribution(410, 20), Distribution(8, 0.8), 'Membrane', comp_type='np')
+membrane_skid = treeNode('Membrane Skid', 'AND', px_array, membrane)
+
+ro_train = treeNode('RO Train', 'AND', diesel_skid, membrane_skid)
 
 main_counter = 0  # hours
 # total_time = 149016  # Full contract
 total_time = 1800
 
 while main_counter < total_time:
-    if engine.running:
-        engine.total_runtime += 1
-        if engine.runtime >= floor(engine.next_failure):
-            engine.switch_state()
-            engine.runtime = 0
+    for child in diesel_skid.children:
+        if child.running == True:
+            child.runtime += 1
+            child.total_runtime += 1
+            if child.runtime >= child.next_failure:
+                child.runtime = 0
+                child.running = False
+                diesel_skid.update_node_status()
+        elif child.running == False:
+            child.repairtime += 1
+            child.total_repairtime += 1
+            if child.repairtime >= child.next_repair_time:
+                child.repairtime = 0
+                child.running = True
+                diesel_skid.update_node_status()
         else:
-            engine.runtime += 1
-    else:
-        engine.total_repairtime += 1
-        if engine.repairtime >= floor(engine.next_repair_time):
-            engine.switch_state()
-            engine.repairtime = 0
-        else:
-            engine.repairtime += 1
+            print('error 1')
+        print(child.name)
+        print(child.total_runtime)
+        print(child.total_repairtime)
     main_counter += 1
-
-print(engine.get_availability())
--- treenode.py
+++ treenode.py
@@ -1,6 +1,55 @@
-from component import Component
-
 class treeNode():
 
-    def __init__(self, node_type, *children):
+    def __init__(self, name, node_function, *children, node_type='np', frac=1):
+        self.name = name
+        self.node_function = node_function
         self.node_type = node_type
+        self.running = True
+        self.frac = frac
+        self.children = children
+        self.total_runtime = 0
+        self.total_repairtime = 0
+
+    def update_node_status(self):
+        if self.node_function == 'AND':
+            not_running = 0
+            for child in self.children:
+                if child.running == False:
+                    not_running += 1
+            if not_running > 0:
+                self.running = False
+            else:
+                self.running = True
+        elif self.node_function == 'OR':
+            running = 0
+            for child in self.children:
+                if child.running == True:
+                    running += 1
+            if running > 0:
+                self.running = True
+
+            else:
+                self.running = False
+        elif self.node_function == 'XofY':
+            running = 0
+            for child in self.children:
+                if child.running == True:
+                    running += 1
+            if running/len(self.children) >= self.frac:
+                self.running = True
+            else:
+                self.running = False
+
+    def print_child_qty(self):
+        print(len(self.children))
+
+    def shutdown_node(self):
+        for child in self.children:
+            child.running = False
+
+    def startup_node(self):
+        for child in self.children:
+            child.running = True
+
+    def get_availability(self):
+        return self.total_runtime/(self.total_runtime+self.total_repairtime)
